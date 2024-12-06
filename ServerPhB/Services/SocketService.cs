cf8b0260b64baa6091c4a0afd947ab8d5aa9be41
Andrey Karagodin <shad-karagodin@ya.ru>
2024-12-06 18:07:21 +0000

    Stage testing changes on client/server interaction

--- ServerPhB/Services/SocketService.cs
+++ ServerPhB/Services/SocketService.cs
@@ -4,6 +4,19 @@ namespace ServerPhB.Services
 {
     public class SocketService
     {
-        
+        public void Connect()
+        {
+            // TODO: Implement this method
+        }
+
+        public void Disconnect()
+        {
+            // TODO: Implement this method
+        }
+
+        public void SendMessage(string message)
+        {
+            // TODO: Implement this method
+        }
     }
 }
