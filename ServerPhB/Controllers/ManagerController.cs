3cef7fd61603633a8f49c45a48509ceef5bc5307
MineShadow7 <shad-karagodin@ya.ru>
2024-12-20 13:37:33 +0300

    Fix capitalization

--- ServerPhB/Controllers/ManagerController.cs
+++ ServerPhB/Controllers/ManagerController.cs
@@ -39,7 +39,7 @@ namespace ServerPhB.Controllers
         [Authorize]
         public async Task<IActionResult> ViewOrderList()
         {
-            var pendingOrders = await _orderService.GetOrdersByStatus("pending");
+            var pendingOrders = await _orderService.GetOrdersByStatus("Pending");
 
             var orderDtos = pendingOrders.Select(order => new OrderDto
             {
