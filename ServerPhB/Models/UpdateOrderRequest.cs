cce5395d48279af2d47764ef3e20816a97159d0d
MineShadow7 <shad-karagodin@ya.ru>
2024-12-24 23:54:33 +0300

    Update OrderUpdate request model to not ask for all fields

--- ServerPhB/Models/UpdateOrderRequest.cs
+++ ServerPhB/Models/UpdateOrderRequest.cs
@@ -7,12 +7,12 @@ namespace ServerPhB.Models
     public class UpdateOrderRequest
     {
         public int OrderID { get; set; }
-        public string Status { get; set; }
-        public string Address { get; set; }
+        public string? Status { get; set; }
+        public string? Address { get; set; }
         public int? DecorationOptionID { get; set; }
-        public string Comments { get; set; }
+        public string? Comments { get; set; }
         public int? TotalPrice { get; set; }
         public int? Quantity { get; set; }
-        public List<IFormFile> Photos { get; set; }
+        public List<IFormFile>? Photos { get; set; }
     }
 }
